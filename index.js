5dd3914bcdc913d73e3954878b2e4bb9487d5897
Prapti Bargir <praptibargir21@gmail.com>
2024-07-24 15:28:06 +0530

    Third api created

--- index.js
+++ index.js
@@ -5,13 +5,31 @@ app.use(express.json())
 
 //This is a temporary db
 const plants=[{
-    "id": 4562,
+    "id": 1,
     "name": "Bamboo",
     "category": "indoor",
     "image": "https://m.media-amazon.com/images/I/518eaTa9hmL._SX679_.jpg",
     "price": "150 Rs",
     "description": "Ugaoo Lucky Bamboo 3 Layer Feng Shui Plant (green color)"
-}]
+},
+{
+    "id": 2,
+    "name": "Pink rose",
+    "category":"outdoor",
+    "image":"https://m.media-amazon.com/images/I/41KC-gJcSrS._SX300_SY300_QL70_FMwebp_.jpg", 
+    "price":"200 Rs", 
+    "description":"Desi Pink Rose Plant Live Flowering with Pot Indigenous Native Fragrant Blooms Indigenous Flora Shrub Petals"
+},
+{
+    "id": 3,
+    "name": "coconut",
+    "category":"outdoor",
+    "image":"https://5.imimg.com/data5/SELLER/Default/2023/5/311018744/YN/DF/FI/2229642/outdoor-coconut-plant-1000x1000.jpg", 
+    "price":"250 Rs", 
+    "description":"Well Watered Well Drained Outdoor Coconut Plant, For Fruits"
+}
+
+]
 
 app.post("/plant",(req,res)=>{
     const {name, 
@@ -86,6 +104,18 @@ app.get("/plant",(req,res)=>{
         success:true,
         data: plants,
         message: "All plants fetched successfully"
+    })
+})
+
+app.get("/plant/:id",(req,res)=>{
+    const {id}=req.params
+
+    const plant=plants.find((p)=>p.id== id)
+
+    res.json({
+        success: plant? true: false,
+        data:plant ||null,
+        message: plant?"Plant fetched successfully":"Plant not Fetched"
     })
 })
 
